6d5793bb9b7503c287b29c65af4c6d6deb88c557
KeyboardFire <andy@keyboardfire.com>
2016-07-17 23:27:21 -0500

    Work with user IDs rather than usernames
    MUTEXES ARE HARD

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use rusqlite::Connection;
 
 extern crate time;
 
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 
 mod message;
 use message::Message;
@@ -22,7 +22,7 @@ pub struct Glavra {
 struct Server {
     glavra: Arc<Mutex<Glavra>>,
     out: ws::Sender,
-    username: Option<String>
+    userid: Option<i32>
 }
 
 impl Glavra {
@@ -33,8 +33,8 @@ impl Glavra {
         conn.execute_batch("BEGIN;
                             CREATE TABLE IF NOT EXISTS messages (
                             id          INTEGER PRIMARY KEY,
+                            userid      INTEGER NOT NULL,
                             text        TEXT NOT NULL,
-                            username    TEXT NOT NULL,
                             timestamp   TEXT NOT NULL
                             );
                             CREATE TABLE IF NOT EXISTS users (
@@ -53,7 +53,7 @@ impl Glavra {
             Server {
                 glavra: arc.clone(),
                 out: out,
-                username: None
+                userid: None
             }
         }).unwrap();
     }
@@ -67,23 +67,25 @@ impl ws::Handler for Server {
 
         let lock = self.glavra.lock().unwrap();
         let mut backlog_query = lock.conn
-            .prepare("SELECT text, username, timestamp FROM messages
+            .prepare("SELECT id, userid, text, timestamp FROM messages
                       ORDER BY id LIMIT 50").unwrap();
 
         for message in backlog_query.query_map(&[], |row| {
                     Message {
-                        text: row.get(0),
-                        username: row.get(1),
-                        timestamp: row.get(2)
+                        id: row.get(0),
+                        userid: row.get(1),
+                        text: row.get(2),
+                        timestamp: row.get(3)
                     }
                 }).unwrap() {
-            self.out.send(serde_json::to_string(&message.unwrap()).unwrap()).unwrap();
+            self.out.send(self.message_json(&message.unwrap(), &lock)).unwrap();
         }
 
         Ok(())
     }
 
     fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
+        println!("on_message called");
         let data = try!(msg.into_text());
         let json: Map<String, Value> = serde_json::from_str(&data[..]).unwrap();
         println!("got message: {:?}", json);
@@ -92,13 +94,15 @@ impl ws::Handler for Server {
         match &msg_type[..] {
 
             "auth" => {
-                let (username, password) =
+                let (username, password, mut userid) =
                     (get_string(&json, "username").unwrap(),
-                     get_string(&json, "password").unwrap());
+                     get_string(&json, "password").unwrap(),
+                     -1);
                 let auth_success = self.glavra.lock().unwrap().conn
-                    .query_row("SELECT password FROM users WHERE username = ?",
+                    .query_row("SELECT id, password FROM users WHERE username = ?",
                                &[&username], |row| {
-                                   let correct_password: String = row.get(0);
+                                   userid = row.get(0);
+                                   let correct_password: String = row.get(1);
                                    password == correct_password
                                }).unwrap_or(false);  // username doesn't exist
 
@@ -109,10 +113,11 @@ impl ws::Handler for Server {
                 try!(self.out.send(serde_json::to_string(&auth_response).unwrap()));
 
                 if auth_success {
-                    self.username = Some(username.clone());
+                    self.userid = Some(userid);
                     let message = Message {
+                        id: -1,
+                        userid: -1,
                         text: format!("{} has connected", username),
-                        username: String::new(),
                         timestamp: time::get_time()
                     };
                     self.send_message(message);
@@ -123,11 +128,20 @@ impl ws::Handler for Server {
                 let (username, password) =
                     (get_string(&json, "username").unwrap(),
                      get_string(&json, "password").unwrap());
+                let success;
 
-                let success = self.glavra.lock().unwrap().conn
-                    .execute("INSERT INTO users (username, password)
-                              VALUES ($1, $2)", &[&username, &password])
-                    .is_ok();
+                {
+                    let lock = self.glavra.lock().unwrap();
+                    success = lock.conn
+                        .execute("INSERT INTO users (username, password)
+                                  VALUES ($1, $2)", &[&username, &password])
+                        .is_ok();
+                    if success {
+                        self.userid = Some(lock.conn
+                            .query_row("SELECT last_insert_rowid()", &[],
+                            |row| row.get(0)).unwrap());
+                    }
+                }
 
                 try!(self.out.send(serde_json::to_string(&ObjectBuilder::new()
                     .insert("type", "register")
@@ -135,10 +149,10 @@ impl ws::Handler for Server {
                     .unwrap()).unwrap()));
 
                 if success {
-                    self.username = Some(username.clone());
                     let message = Message {
+                        id: -1,
+                        userid: -1,
                         text: format!("{} has connected", username),
-                        username: String::new(),
                         timestamp: time::get_time()
                     };
                     self.send_message(message);
@@ -147,8 +161,9 @@ impl ws::Handler for Server {
 
             "message" => {
                 let message = Message {
+                    id: -1,
+                    userid: self.userid.clone().unwrap(),
                     text: get_string(&json, "text").unwrap(),
-                    username: self.username.clone().unwrap(),
                     timestamp: time::get_time()
                 };
                 self.send_message(message);
@@ -162,10 +177,13 @@ impl ws::Handler for Server {
 
     fn on_close(&mut self, _: ws::CloseCode, _: &str) {
         println!("client disconnected");
-        if self.username.is_some() {
+        if self.userid.is_some() {
             let message = Message {
-                text: format!("{} has disconnected", self.username.clone().unwrap()),
-                username: String::new(),
+                id: -1,
+                userid: -1,
+                text: format!("{} has disconnected",
+                    self.get_username(self.userid.clone().unwrap(),
+                        &self.glavra.lock().unwrap()).unwrap()),
                 timestamp: time::get_time()
             };
             self.send_message(message);
@@ -177,11 +195,28 @@ impl ws::Handler for Server {
 impl Server {
 
     fn send_message(&mut self, message: Message) {
-        self.out.broadcast(serde_json::to_string(&message).unwrap()).unwrap();
-        self.glavra.lock().unwrap().conn.execute("INSERT INTO messages
-                (text, username, timestamp) VALUES ($1, $2, $3)",
-                &[&message.text, &message.username, &message.timestamp])
+        let lock = self.glavra.lock().unwrap();
+        self.out.broadcast(self.message_json(&message, &lock)).unwrap();
+        lock.conn.execute("INSERT INTO messages
+                (userid, text, timestamp) VALUES ($1, $2, $3)",
+                &[&message.userid, &message.text, &message.timestamp])
             .unwrap();
+    }
+
+    fn get_username(&self, userid: i32, lock: &MutexGuard<Glavra>)
+            -> Result<String, rusqlite::Error> {
+        if userid == -1 { return Ok(String::new()); }
+        lock.conn.query_row("SELECT username FROM users
+            WHERE id = ?", &[&userid], |row| { row.get(0) })
+    }
+
+    fn message_json(&self, message: &Message, lock: &MutexGuard<Glavra>) -> String {
+        serde_json::to_string(&ObjectBuilder::new()
+            .insert("type", "message")
+            .insert("username", self.get_username(message.userid, lock).unwrap())
+            .insert("text", &message.text)
+            .insert("timestamp", message.timestamp.sec)
+            .unwrap()).unwrap()
     }
 
 }
--- src/message.rs
+++ src/message.rs
@@ -1,40 +1,10 @@
-extern crate serde;
-
 extern crate time;
 use time::Timespec;
 
 #[derive(Clone)]
 pub struct Message {
+    pub id: i32,
+    pub userid: i32,
     pub text: String,
-    pub username: String,
     pub timestamp: Timespec
-}
-
-impl serde::Serialize for Message {
-    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-            where S: serde::Serializer {
-        serializer.serialize_struct("Message", MessageMapVisitor {
-            value: self,
-            state: 0
-        })
-    }
-}
-
-struct MessageMapVisitor<'a> {
-    value: &'a Message,
-    state: u8
-}
-
-impl<'a> serde::ser::MapVisitor for MessageMapVisitor<'a> {
-    fn visit<S>(&mut self, serializer: &mut S) -> Result<Option<()>, S::Error>
-            where S: serde::Serializer {
-        self.state += 1;
-        match self.state {
-            1 => Ok(Some(try!(serializer.serialize_struct_elt("type", String::from("message"))))),
-            2 => Ok(Some(try!(serializer.serialize_struct_elt("text", &self.value.text)))),
-            3 => Ok(Some(try!(serializer.serialize_struct_elt("username", &self.value.username)))),
-            4 => Ok(Some(try!(serializer.serialize_struct_elt("timestamp", &self.value.timestamp.sec)))),
-            _ => Ok(None)
-        }
-    }
 }
